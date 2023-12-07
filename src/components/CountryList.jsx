4c79d5c42725db8ac407eb5023642327111ecb9b
abc <you@example.com>
2023-12-07 16:09:38 +0330

    ListCountriesImplement-version-1

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,13 +1,18 @@
 import styles from "./CountryList.module.css";
 import CountryItem from "./CountryItem";
 export default function CountryList({ cities }) {
-    const countries =[]
-    countries = cities.reduce((acc,cur)=>acc===cur)
+    let countries = [];
+
+    countries = cities.reduce((acc, cur) => {
+        if (acc.some((_) => _.country === cur.country)) return acc;
+       //if(acc.map(_=>_.country).includes(cur.country)) return acc ---By map and includes 
+        else return [...acc, cur];
+    }, []);
+    console.log(countries);
     return (
         <div className={styles.countryList}>
             {countries.map((country) => (
-                
-                <CountryItem country={country} />
+                <CountryItem country={country} key={country.id} />
             ))}
         </div>
     );
